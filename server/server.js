3d4288242e23400119ad29d4dda2b3d4c977c0a7
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-01-12 14:13:30 +0000

    added auth0 for authentication login using gmail logined

--- server/server.js
+++ server/server.js
@@ -1,7 +1,27 @@
 import express from 'express'
+import { auth } from 'express-openid-connect';
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and     /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
 app.listen(port, () => console.log(`server listening on port ${port}!`))
